6f782c81403d2a929d0a80d515c3cc026683d940
MICHICOM <mitiaki.com.320@gmail.com>
2022-06-13 18:28:50 +0900

    Toggleの骨組み

---
+++ Hypothesis/Assets/QMMaterial/Components/AbilityCount.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCount : MonoBehaviour
+{
+    public int AbilityLimit = 1;
+    //set limit of all of Ability
+
+    public int AJLimit = 0;
+    public int DashLimit = 0;
+    public int GrappleLimit = 0;
+    //set limit of Ability
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
