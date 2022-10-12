38864a3b96eee6092336ad53102155265cbac4f9
MICHICOM <mitiaki.com.320@gmail.com>
2022-10-12 19:15:46 +0900

    GUI切り替え実装（一時保存用）

--- Hypothesis/Assets/QMMaterial/Components/QMmaster.cs
+++ Hypothesis/Assets/QMMaterial/Components/QMmaster.cs
@@ -6,11 +6,28 @@ public class QMmaster : MonoBehaviour
 {
     public GameObject obj;
     bool QMshow = false;
+    public GameObject Camera;
     public PlayerController robocontroller;
+    public GameObject APanel;
+    public GameObject MPanel;
+    public GameObject TPanel;
+    public GameObject EDPanel;
+
+    public GameObject AIco;
+    public GameObject MIco;
+    public GameObject TIco;
+    public GameObject EDIco;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Canvas>().enabled = false;
+        APanel.SetActive(false);
+        MPanel.SetActive(false);
+        TPanel.SetActive(false);
+        EDPanel.SetActive(false);
     }
 
     
@@ -18,7 +35,7 @@ public class QMmaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //QMÇÃON,OffÇä«óù
         if (Input.GetButtonDown("OpenQM"))
         {
             if (QMshow == false)
@@ -38,8 +55,38 @@ public class QMmaster : MonoBehaviour
                 Time.timeScale = 1f;
             }
         }
+
+
+        //QMÇÃÉAÉCÉRÉìïœâªÇÃä«óù
+        if (APanel.activeSelf==true)
+        {
+            Debug.Log("AbillityPanel is enabled");
+
+            Transform Cam = Camera.transform;
+            Transform AIT = AIco.transform;
+            Transform MIT = MIco.transform;
+            Transform TIT = TIco.transform;
+            Transform EDIT = EDIco.transform;
+
+            Vector2 IcosY = Cam.position;
+            IcosY.y += 6;
+
+            AIT.position = IcosY;
+            MIT.position = IcosY;
+            TIT.position = IcosY;
+            EDIT.position = IcosY;
+
+            AIco.transform.localScale = Vector3.one*0.1f;
+        }
+
+    }
+
+    private void OnEnable()
+    {
         
     }
+
+
 
 
 
